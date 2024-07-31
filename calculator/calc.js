80825be8739b6373e37f4796db9d82aaa826f740
SSDAbdullah <Abdulla.basit@outlook.com>
2024-07-31 22:39:39 +0500

    start

---
+++ calculator/calc.js
@@ -0,0 +1,95 @@
+//calculate
+var operand1 = "";
+var operator = "";
+var pushValue = "";
+
+function calculate(leftOperand, operator, rightOperand, valueBox) {
+  var left = Number(leftOperand)
+  var right = Number(rightOperand)
+
+  if (operator === "+") {
+    var result = left   + right;
+    
+    valueBox.val(result);
+  } else {
+    if (operator === "-") {
+      var result = left - right;
+      valueBox.val(result);
+    } else {
+      if (operator === "*") {
+        var result = left * right;
+        valueBox.val(result);
+      } else {
+        if (operator === "/") {
+          var result = left / right;
+          valueBox.val(result);
+        } else {
+          if (operator === "%") {
+            var result = left % right;
+            valueBox.val(result);
+          } else {
+            valueBox.val("error!!!!!!!");
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+
+$(".btn").click(function () {
+  var btnValue = $(this).text();
+  var valueBox = $("#valueBox");
+
+  
+  switch (btnValue) {
+    case "=":
+      var operand2 = pushValue.slice(1, pushValue.length);
+      pushValue = "";
+      calculate(operand1, operator, operand2, valueBox);
+      break;
+    case "/":
+      operator = "/";
+      operand1 = pushValue;
+      pushValue = "";
+      pushValue = pushValue.concat(btnValue);
+      valueBox.val(pushValue);
+      break;
+
+    case "+":
+      operator = "+";
+      operand1 = pushValue;
+      pushValue = "";
+      pushValue = pushValue.concat(btnValue);
+      valueBox.val(pushValue);
+
+      break;
+    case "*":
+      operator = "*";
+      operand1 = pushValue;
+      pushValue = "";
+      pushValue = pushValue.concat(btnValue);
+      valueBox.val(pushValue);
+      break;
+    case "-":
+      operator = "-";
+      operand1 = pushValue;
+      pushValue = "";
+      pushValue = pushValue.concat(btnValue);
+      valueBox.val(pushValue);
+
+    case "%":
+      operator = "%";
+      operand1 = pushValue;
+      pushValue = "";
+      pushValue = pushValue.concat(btnValue);
+      valueBox.val(pushValue);
+      break;
+    default:
+      //pushValue.push(btnValue)
+      pushValue = pushValue+btnValue;
+      valueBox.val(pushValue);
+  }
+});
